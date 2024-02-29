404f6abf99f0744768b1290557df90c0813e9260
mandeukJeong <wjdalstj137@gmail.com>
2024-02-29 15:32:13 +0900

    feat: 페이지 분리, reset css 적용

--- jaksim/src/App.js
+++ jaksim/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/main/HomePage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
 
 const App = () => {
-  return <div>작심하루</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
 };
 
 export default App;
