1fe2cb7da7eb45a33a827610e93d996d13f578a5
mandeukJeong <wjdalstj137@gmail.com>
2024-03-06 00:03:50 +0900

    style: home desktop

--- jaksim/src/pages/main/HomePage.js
+++ jaksim/src/pages/main/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import HeaderComponent from '../../components/common/HeaderComponent';
 import IconImg from './../../assets/img/icon_wrap.png';
+import JayImg from './../../assets/img/Jay.png';
+import GroupImg from './../../assets/img/group.png';
+import GoalImg from './../../assets/img/goal.png';
+import FriendsImg from './../../assets/img/add_friends.png';
 
 const MainWrap = styled.div`
   width: 100%;
@@ -10,10 +15,148 @@ const MainWrap = styled.div`
 const IconWrap = styled.div`
   margin-top: 55px;
   width: 100%;
-  height: 800px;
+  height: 74vh;
   background-image: url(${IconImg});
-  background-size: contain;
+  background-size: cover;
+  background-position: center;
   background-repeat: no-repeat;
+`;
+
+const IntroduceWrap = styled.div`
+  background-color: #333245;
+  width: 100%;
+  height: 600px;
+  padding: 80px 80px 40px 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  position: relative;
+
+  img {
+    position: absolute;
+    top: 80px;
+    width: 10%;
+    max-width: 130px;
+    padding: 20px 25px;
+    background: #4f59ca;
+    border-radius: 50%;
+    z-index: 1;
+  }
+
+  p {
+    line-height: 30px;
+    font-size: 20px;
+  }
+`;
+
+const IntroduceTextWrap = styled.div`
+  width: 60%;
+  background-color: white;
+  padding: 100px;
+  border-radius: 100px;
+  position: absolute;
+  top: 200px;
+`;
+
+const FuncWrap = styled.div`
+  width: 100%;
+  height: 600px;
+  text-align: center;
+  padding: 60px 80px;
+  background-color: white;
+
+  h4 {
+    color: #4f59ca;
+    font-size: 25px;
+    margin-bottom: 50px;
+  }
+
+  span {
+    font-weight: 700;
+  }
+`;
+
+const FuncElement = styled.div`
+  display: flex;
+  width: 100%;
+
+  > div {
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  p {
+    background-color: rgba(78, 92, 195, 0.5);
+    color: white;
+    padding: 10px 50px;
+    border-radius: 30px;
+    font-size: 20px;
+    font-weight: 700;
+  }
+`;
+
+const FuncImgWrap = styled.div`
+  width: 70%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 50px;
+
+  img {
+    width: 100%;
+  }
+`;
+
+const JoinWrap = styled.div`
+  width: 100%;
+  height: 600px;
+  background-color: #fef9ee;
+  text-align: center;
+  padding: 150px;
+  position: relative;
+
+  p {
+    font-size: 25px;
+    line-height: 40px;
+    color: #4e5cc3;
+    margin-bottom: 50px;
+  }
+
+  a {
+    text-decoration: none;
+    background-color: #4e5cc3;
+    color: white;
+    font-weight: 700;
+    font-size: 20px;
+    padding: 20px 40px;
+    border-radius: 30px;
+  }
+`;
+
+const Circle = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  position: absolute;
+  background-color: ${(props) => props.color};
+  top: ${(props) => props.top};
+  bottom: ${(props) => props.bottom};
+  left: ${(props) => props.left};
+  right: ${(props) => props.right};
+  border: ${(props) => (props.border ? `1px solid ${props.border}` : '')};
+  z-index: ${(props) => props.index};
+`;
+
+const DesignWrap = styled.div`
+  width: 100%;
+  height: 150px;
+  background-color: white;
 `;
 
 const HomePage = () => {
@@ -21,57 +164,66 @@ const HomePage = () => {
     <MainWrap>
       <HeaderComponent />
       <IconWrap />
-      {/* <div class="introduce-wrap">
-            <div class="circle yellow-intro"></div>
-            <div class="circle border-yellow"></div>
-            <div class="circle border-purple"></div>
-            <div class="circle shadow-purple"></div>
-            <img src="../img/Jay.png">
-            <div>
-                <p>
-                    다양한 사람들과 함께 만나며 마음껏 지식을 공유해 보세요.<br>
-                    작심하루가 여러분들의 앞으로의 여정을 응원하겠습니다.<br>
-                    From. 에디터 제이
-                </p>
-            </div>
-        </div>
-        <div class="func-wrap">
-            <h4>
-                <span>작심하루</span>에서는 가능해요.
-            </h4>
-            <div class="func-element">
-                <div>
-                    <div class="func-img">
-                        <img src="../img/group.png">
-                    </div>
-                    <p>그룹스터디</p>
-                </div>
-                <div>
-                    <div class="func-img">
-                        <img src="../img/goal.png">
-                    </div>
-                    <p>목표 설정</p>
-                </div>
-                <div>
-                    <div class="func-img">
-                        <img src="../img/add-friends.png">
-                    </div>
-                    <p>친구 추가</p>
-                </div>
-            </div>
-        </div>
-        <div class="join-wrap">
-            <p>
-                작심하루가 도와줄게요!<br>
-                다양한 사람들을 만나 함께 공부하고<br>
-                계속 성장 하세요!
-            </p>
-            <a href="#">5초만에 작심하루러 ✍🏻</a>
-            <div class="circle pink"></div>
-            <div class="circle purple"></div>
-            <div class="circle yellow"></div>
-        </div>
-        <div class="design-wrap"></div> */}
+      <IntroduceWrap>
+        <Circle color="#FEC03A" right="150px" bottom="-20px" />
+        <Circle border="#FEC03A" right="200px" bottom="-50px" />
+        <Circle border="#AD9AF5" left="220px" top="150px" />
+        <Circle
+          color="rgba(104, 79, 202, 0.4)"
+          left="270px"
+          top="200px"
+          index="1"
+        />
+        <img src={JayImg} alt="Jay" />
+        <IntroduceTextWrap>
+          <p>
+            다양한 사람들과 함께 만나며 마음껏 지식을 공유해 보세요.
+            <br />
+            작심하루가 여러분들의 앞으로의 여정을 응원하겠습니다.
+            <br />
+            From. 에디터 제이
+          </p>
+        </IntroduceTextWrap>
+      </IntroduceWrap>
+      <FuncWrap>
+        <h4>
+          <span>작심하루</span>에서는 가능해요.
+        </h4>
+        <FuncElement>
+          <div>
+            <FuncImgWrap>
+              <img src={GroupImg} alt="그룹스터디" />
+            </FuncImgWrap>
+            <p>마이페이지</p>
+          </div>
+          <div>
+            <FuncImgWrap>
+              <img src={GoalImg} alt="목표 설정" />
+            </FuncImgWrap>
+            <p>목표 설정</p>
+          </div>
+          <div>
+            <FuncImgWrap>
+              <img src={FriendsImg} alt="친구 추가" />
+            </FuncImgWrap>
+            <p>커뮤니티</p>
+          </div>
+        </FuncElement>
+      </FuncWrap>
+      <JoinWrap>
+        <p>
+          작심하루가 도와줄게요!
+          <br />
+          다양한 사람들을 만나 함께 공부하고
+          <br />
+          계속 성장 하세요!
+        </p>
+        <Link to="/register">5초만에 작심하루러 ✍🏻</Link>
+        <Circle color="#FFD4BD80" right="80px" bottom="80px" />
+        <Circle color="#684FCA" left="150px" bottom="-50px" />
+        <Circle color="#FEC03A" left="200px" bottom="-80px" />
+      </JoinWrap>
+      <DesignWrap />
     </MainWrap>
   );
 };
