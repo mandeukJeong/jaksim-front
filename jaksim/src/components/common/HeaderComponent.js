2ec3639c02f29c7971697823b5cbe225275579c1
mandeukJeong <wjdalstj137@gmail.com>
2024-03-06 16:59:37 +0900

    feat: header 메뉴 표출

--- jaksim/src/components/common/HeaderComponent.js
+++ jaksim/src/components/common/HeaderComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -47,6 +47,7 @@ const MenuWrap = styled.div`
   display: flex;
   align-items: center;
   gap: 30px;
+
   a {
     text-decoration: none;
     color: #000000;
@@ -62,9 +63,44 @@ const MenuWrap = styled.div`
   }
 `;
 
+const OptionWrap = styled.div`
+  width: 18%;
+  left: 100px;
+  border: 1px solid #a9a9a9;
+  background-color: white;
+  position: fixed;
+  z-index: 10;
+  top: 50px;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
+
+  ul {
+    box-sizing: border-box;
+    display: flex;
+    padding: 15px 20px;
+    width: 100%;
+    justify-content: center;
+  }
+
+  li {
+    border-right: 1px solid #a9a9a9;
+
+    &:last-child {
+      border-right: none;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 12px;
+    padding: 0 10px;
+    color: #000000;
+  }
+`;
+
 const HeaderComponent = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
+  const [isRenderMenu, setIsRenderMenu] = useState(false);
 
   const onLogout = () => {
     logout()
@@ -85,27 +121,50 @@ const HeaderComponent = () => {
   };
 
   return (
-    <HeaderWrap>
-      <SelectWrap>
-        <Link to="/">HARU</Link>
-        <CategoryWrap>
-          <FontAwesomeIcon icon={faBars} />
-          <p>전체 카테고리</p>
-        </CategoryWrap>
-      </SelectWrap>
-      <MenuWrap>
-        {user.isLogin ? (
-          <Link to="/">{user.userInfo.nickname}</Link>
-        ) : (
-          <Link to="/register">회원가입</Link>
-        )}
-        {user.isLogin ? (
-          <button onClick={onLogout}>로그아웃</button>
-        ) : (
-          <Link to="/login">로그인</Link>
-        )}
-      </MenuWrap>
-    </HeaderWrap>
+    <>
+      <HeaderWrap>
+        <SelectWrap>
+          <Link to="/">HARU</Link>
+          <CategoryWrap onClick={() => setIsRenderMenu(!isRenderMenu)}>
+            <FontAwesomeIcon icon={faBars} />
+            <p>전체 카테고리</p>
+          </CategoryWrap>
+        </SelectWrap>
+        <MenuWrap>
+          {user.isLogin ? (
+            <Link to="/">{user.userInfo.nickname}</Link>
+          ) : (
+            <Link to="/register">회원가입</Link>
+          )}
+          {user.isLogin ? (
+            <button onClick={onLogout}>로그아웃</button>
+          ) : (
+            <Link to="/login">로그인</Link>
+          )}
+        </MenuWrap>
+      </HeaderWrap>
+      {isRenderMenu && (
+        <OptionWrap>
+          <ul>
+            <li>
+              <Link to="/" onClick={() => setIsRenderMenu(!isRenderMenu)}>
+                작심계획
+              </Link>
+            </li>
+            <li>
+              <Link to="/" onClick={() => setIsRenderMenu(!isRenderMenu)}>
+                커뮤니티
+              </Link>
+            </li>
+            <li>
+              <Link to="/" onClick={() => setIsRenderMenu(!isRenderMenu)}>
+                마이페이지
+              </Link>
+            </li>
+          </ul>
+        </OptionWrap>
+      )}
+    </>
   );
 };
 
