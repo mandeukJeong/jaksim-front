16cf42935393a526fc95d9b2dc8bd049c098a8e1
mandeukJeong <wjdalstj137@gmail.com>
2024-03-11 23:55:11 +0900

    style: board List desktop

--- jaksim/src/pages/board/FreeBoardPage.js
+++ jaksim/src/pages/board/FreeBoardPage.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import HeaderComponent from './../../components/common/HeaderComponent';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const MainWrap = styled.div`
   width: 100%;
@@ -43,19 +43,121 @@ const ProfileWrap = styled.div`
 
 const ContentWrap = styled.div`
   margin-top: 90px;
+  padding: 40px 0;
   display: flex;
   width: 90%;
+  gap: 40px;
 `;
 
-const MenuWrap = styled.div``;
+const MenuWrap = styled.div`
+  width: 12%;
+  margin-top: 15px;
+  li {
+    border-bottom: 1px solid #d9d9d9;
+    margin-bottom: 40px;
+  }
 
-const BoardWrap = styled.div``;
+  a {
+    text-decoration: none;
+    color: #000000;
+    font-size: 16px;
+    font-weight: 500;
+  }
+`;
 
-const CategoryWrap = styled.div``;
+const BoardWrap = styled.div`
+  width: 88%;
+`;
 
-const SearchWrap = styled.div``;
+const CategoryWrap = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: flex-end;
+  gap: 15px;
 
-const ListWrap = styled.div``;
+  h3 {
+    font-weight: 700;
+    font-size: 28px;
+  }
+
+  p {
+    font-size: 15px;
+    color: #999999;
+    font-weight: 400;
+  }
+`;
+
+const SearchWrap = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #bebebe;
+
+  div {
+    cursor: pointer;
+  }
+
+  input {
+    padding: 5px 0;
+    margin-right: 5px;
+    border: none;
+  }
+
+  input::placeholder {
+    color: #d8d8d8;
+  }
+
+  button {
+    cursor: pointer;
+    font-size: 15px;
+    font-weight: 700;
+    background-color: rgba(217, 217, 217, 0.8);
+    border: none;
+    color: #ffffff;
+    padding: 10px 25px;
+  }
+`;
+
+const ListWrap = styled.div`
+  width: 100%;
+  background-color: rgba(216, 216, 216, 0.1);
+
+  li {
+    display: flex;
+    padding: 15px 30px;
+    align-items: center;
+    gap: 20px;
+    border-bottom: 1px solid #d9d9d9;
+
+    &:first-child {
+      border-top: 1px solid #d9d9d9;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 15px;
+    color: #000000;
+
+    strong {
+      color: #4f59ca;
+      font-weight: 700;
+    }
+
+    span {
+      color: #a9a9a9;
+      font-weight: 700;
+    }
+  }
+
+  p {
+    margin-top: 5px;
+    font-size: 12px;
+  }
+`;
 
 const FreeBoardPage = () => {
   return (
@@ -96,7 +198,10 @@ const FreeBoardPage = () => {
               <p>알아두면 유익한 서비스 꿀팁 참고하세요!</p>
             </CategoryWrap>
             <SearchWrap>
-              <input type="text" placeholder="검색어를 입력하세요" />
+              <div>
+                <input type="text" placeholder="검색어를 입력하세요" />
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </div>
               <button>글 작성하기</button>
             </SearchWrap>
             <ListWrap>
@@ -106,16 +211,57 @@ const FreeBoardPage = () => {
                     icon={faUser}
                     style={{
                       backgroundColor: '#000000',
-                      fontSize: '25px',
+                      fontSize: '20px',
                       borderRadius: '50%',
-                      padding: '10px 11.57px',
+                      padding: '8px 9.25px',
                       color: 'FFFFFF',
                     }}
                   />
-                  <Link>
-                    <strong>[공지]</strong> 작심하루 오픈을 축하해주세요!{' '}
-                    <span>[5]</span>
-                  </Link>
+                  <div>
+                    <Link>
+                      <strong>[공지]</strong> 작심하루 오픈을 축하해주세요!{' '}
+                      <span>[5]</span>
+                    </Link>
+                    <p>운영자</p>
+                  </div>
+                </li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    style={{
+                      backgroundColor: '#000000',
+                      fontSize: '20px',
+                      borderRadius: '50%',
+                      padding: '8px 9.25px',
+                      color: 'FFFFFF',
+                    }}
+                  />
+                  <div>
+                    <Link>
+                      <strong>[공지]</strong> 작심하루 오픈을 축하해주세요!{' '}
+                      <span>[5]</span>
+                    </Link>
+                    <p>운영자</p>
+                  </div>
+                </li>
+                <li>
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    style={{
+                      backgroundColor: '#000000',
+                      fontSize: '20px',
+                      borderRadius: '50%',
+                      padding: '8px 9.25px',
+                      color: 'FFFFFF',
+                    }}
+                  />
+                  <div>
+                    <Link>
+                      <strong>[공지]</strong> 작심하루 오픈을 축하해주세요!{' '}
+                      <span>[5]</span>
+                    </Link>
+                    <p>운영자</p>
+                  </div>
                 </li>
               </ul>
             </ListWrap>
